28f6eba98b234ac4d41d4219734487c9a3255ec7
Sebastian <a13sebja@student.his.se>
2014-03-05 16:12:20 +0100

    Fade & snake

---
+++ Lucid/Lucid/Fade.cpp
@@ -0,0 +1,73 @@
+#include "Fade.h"
+
+
+Fade::Fade(sf::Texture* texture, sf::RenderTexture &renderTexture):
+	mTexture(texture)
+{
+	mFadeSpeed = 20;
+	mSize.x = renderTexture.getSize().x;
+	mSize.y = renderTexture.getSize().y;
+	mFadeIn = false;
+	mFadeOut = false;
+	mMapID = 0;
+	mAlpha = 0;
+}
+
+
+Fade::~Fade(void)
+{
+}
+
+void Fade::render(sf::RenderWindow &window)
+{
+	sf::RectangleShape r;
+	r.setTexture(mTexture);
+	r.setTextureRect(sf::IntRect(0, 0, 1920, 1080));
+	r.setPosition(0, 0);
+	r.setSize(mSize);
+	r.setFillColor(sf::Color(255, 255, 255, mAlpha));
+	window.draw(r);
+}
+
+void Fade::fadeOut(int newMapID)
+{
+	if (!mFadeOut)
+	{
+		mMapID = newMapID;
+		mFadeOut = true;
+		mFadeIn = false;
+		mAlpha = 0;
+	}
+}
+
+void Fade::fadeIn()
+{
+	mFadeOut = false;
+	mFadeIn = true;
+	mAlpha = 255;
+}
+
+int Fade::tick()
+{
+	if (mFadeOut)
+	{
+		mAlpha += mFadeSpeed;
+		if (mAlpha > 255)
+		{
+			mAlpha = 255;
+			mFadeOut = false;
+			fadeIn();
+			return mMapID;
+		}
+	}
+	else if (mFadeIn)
+	{
+		mAlpha -= mFadeSpeed;
+		if (mAlpha < 0)
+		{
+			mAlpha = 0;
+			mFadeIn = false;
+		}
+	}
+	return 0;
+}
---
+++ Lucid/Lucid/PortalFade.cpp
@@ -0,0 +1,71 @@
+#include "PortalFade.h"
+
+
+PortalFade::PortalFade(sf::Texture* texture, sf::RenderTexture &renderTexture):
+	mTexture(texture)
+{
+	mFadeSpeed = 20;
+	mSize.x = renderTexture.getSize().x;
+	mSize.y = renderTexture.getSize().y;
+	mFadeIn = false;
+	mFadeOut = false;
+	mAlpha = 0;
+	mPosition = sf::FloatRect(0, 0, 0, 0);
+}
+
+
+PortalFade::~PortalFade(void)
+{
+}
+
+void PortalFade::render(sf::RenderWindow &window)
+{
+	sf::RectangleShape r;
+	r.setTexture(mTexture);
+	r.setTextureRect(sf::IntRect(0, 0, 1920, 1080));
+	r.setPosition(0, 0);
+	r.setSize(mSize);
+	r.setFillColor(sf::Color(255, 255, 255, mAlpha));
+	window.draw(r);
+}
+
+void PortalFade::fadeOut(sf::FloatRect position)
+{
+	if (!mFadeOut)
+	{
+		mPosition = position;
+		mFadeOut = true;
+		mFadeIn = false;
+		mAlpha = 0;
+	}
+}
+
+void PortalFade::fadeIn()
+{
+	mFadeOut = false;
+	mFadeIn = true;
+	mAlpha = 255;
+}
+
+sf::FloatRect PortalFade::tick()
+{
+	if (mFadeOut)
+	{
+		mAlpha += mFadeSpeed;
+		if (mAlpha > 255)
+		{
+			fadeIn();
+			return mPosition;
+		}
+	}
+	else if (mFadeIn)
+	{
+		mAlpha -= mFadeSpeed;
+		if (mAlpha < 0)
+		{
+			mAlpha = 0;
+			mFadeIn = false;
+		}
+	}
+	return sf::FloatRect(0, 0, 0, 0);
+}
